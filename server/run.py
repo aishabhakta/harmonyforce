ea31cd2808c16f1e5fff243840ff1d4198462435
aishabhakta <aishabhakta@gmail.com>
2025-04-08 13:56:36 -0400

    edits

--- server/run.py
+++ server/run.py
@@ -7,13 +7,13 @@
 
 
 from app import create_app
-from app.payments.stripe_webhooks import webhook_bp  
+# from app.payments.stripe_webhooks import webhook_bp  
 
 # Create main app
 app = create_app()
 
 # Register Webhook Blueprint
-app.register_blueprint(webhook_bp) 
+# app.register_blueprint(webhook_bp) 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000) 
